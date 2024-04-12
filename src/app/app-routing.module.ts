8c4bed31560cce5d7539f66541f5799931ee315f
AJMG-95 <antoniojesus.marchena@iesdonana.org>
2024-04-12 19:04:21 +0200

    CountriesRoutingModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,25 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
 import { AboutPageComponent } from "./shared/pages/about-page/about-page.component";
+import { ContactPageComponent } from "./shared/pages/contact-page/contact-page.component";
 import { HomePageComponent } from "./shared/pages/home-page/home-page.component";
 
 const routes: Routes = [
   {
     path: '',
-    component: HomePageComponent
+    component: HomePageComponent,
   },
   {
     path: 'about',
-    component: AboutPageComponent
+    component: AboutPageComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactPageComponent,
+  },
+  {
+    path: 'countries',
+    component: ContactPageComponent,
   },
   {
     path: '**',
